7368d465b69c4b049fdfb8137c3b16fc0f8e3e04
Micaela Marques <micaellagrangeiro@gmail.com>
2024-03-13 14:17:54 -0300

    feat: start project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as router from "./router.js"
+
+const router = new Router()
+router.add("/", "../pages/home.html")
+router.add("/exploration", "../pages/exploration.html")
+router.add("/universe", "../pages/universe.html")
+
+
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
